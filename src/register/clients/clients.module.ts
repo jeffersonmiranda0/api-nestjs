b2d9f3b274d0f2b5260af351f709ff4fd72087b2
Jefferson Miranda <jeffersonmiranda0@gmail.com>
2020-03-18 23:57:23 -0300

    commit testes com websocket

--- src/register/clients/clients.module.ts
+++ src/register/clients/clients.module.ts
@@ -3,10 +3,10 @@ import { ClientsController } from './clients.controller';
 import { ClientsService } from './clients.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Clients } from './clients.entity';
-
 @Module({
   imports: [TypeOrmModule.forFeature([Clients])],
   controllers: [ClientsController],
   providers: [ClientsService],
+  exports: [],
 })
 export class ClientsModule {}
